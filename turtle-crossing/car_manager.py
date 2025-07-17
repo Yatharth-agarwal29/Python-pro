7df80dabd6599872dc87917ae276c325325814f5
yatharth <yatharth.agarwal10@gmail.com>
2025-07-17 23:37:08 +0530

    G2 part 2 done

--- turtle-crossing/car_manager.py
+++ turtle-crossing/car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+from  random import *
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    
+    def __init__(self):
+       self.all_cars=[]
+
+    def create_car(self):
+        newcar= Turtle("Square")
+        newcar.shapesize(stretch_len=1, stretch_wid=2)
+        newcar.penup()
+        newcar.color(random.choice(COLORS))
+        car_y = randint(-250,250)
+        newcar.goto(300,car_y)
+        self.all_cars.append(newcar)
+
+    def move_car(self);
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+            
+            
