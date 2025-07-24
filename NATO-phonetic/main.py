622cabff5827065ebc6e332a8910014a7ce03a22
yatharth <yatharth.agarwal10@gmail.com>
2025-07-24 12:18:05 +0530

    Error handleing , done

--- NATO-phonetic/main.py
+++ NATO-phonetic/main.py
@@ -1,8 +1,13 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def genrate_phonetic():
+    word = input("Enter a word: ").upper()
+    try :
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, Alphabets only ")
+        genrate_phonetic()
+    print(output_list)
+genrate_phonetic()
